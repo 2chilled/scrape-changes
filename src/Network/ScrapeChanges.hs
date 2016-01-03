3ea3b53c34a1d4806a5c6db28c9fa17d4efb1b5c
Matthias Herrmann <matthias.mh.herrmann@googlemail.com>
2016-01-03 12:11:09 +0100

    Tell the user if the callback was called

--- src/Network/ScrapeChanges.hs
+++ src/Network/ScrapeChanges.hs
@@ -13,7 +13,6 @@ import qualified System.Cron.Schedule as CronSchedule
 import Control.Lens
 import qualified Data.ByteString.Lazy.Internal as ByteString
 import qualified Network.Wreq as Http
-import Control.Monad (when)
 import qualified Control.Concurrent.Async as Async
 import qualified System.Log.Logger as Log
 import Data.Hashable (Hashable)
@@ -22,12 +21,14 @@ import qualified Data.Hashable as Hashable
 type Url = String
 type Scraper t = ByteString.ByteString -> t
 
+data ScrapeResult t = CallbackCalled t | CallbackNotCalled t
+
 -- TODO 
 -- add a function for cleaning up the written hashes
 -- review the hash functions
 
-scrape :: Hashable t => ScrapeConfig t -> Scraper t -> Either [ValidationError] (IO t)
-scrape sc s = let result = const scrapeOrchestration <$> validateScrapeConfig sc
+scrape :: Hashable t => ScrapeConfig t -> Scraper t -> Either [ValidationError] (IO (ScrapeResult t))
+scrape sc s = let result = scrapeOrchestration <$ validateScrapeConfig sc
               in result ^. Validation._Either
   where scrapeOrchestration = 
           let unpackResponse = (^. Http.responseBody)
@@ -38,12 +39,12 @@ scrape sc s = let result = const scrapeOrchestration <$> validateScrapeConfig sc
           in do (response', latestHashedResponse) <- Async.concurrently response (readLatestHash sc)
                 let currentHashedResponse = show . Hashable.hash $ response'
                 let hashesAreDifferent = latestHashedResponse /= currentHashedResponse
-                let saveHashIfSomethingHasChanged = let saveHashLog = Log.infoM thisModule $ "Saved new hash for url '" ++ urlToRequest ++ "'"
-                                                        saveHash' = saveHash sc latestHashedResponse <* saveHashLog
-                                                    in  when hashesAreDifferent saveHash' 
+                let saveHash' = let saveHashLog = Log.infoM thisModule $ "Saved new hash for url '" ++ urlToRequest ++ "'"
+                                in  saveHash sc latestHashedResponse <* saveHashLog
                 let executeCallbackConfig' = executeCallbackConfig (sc ^. scrapeInfoCallbackConfig) response'
-                _ <- Async.concurrently saveHashIfSomethingHasChanged executeCallbackConfig'
-                return response'
+                let saveHashAndExecuteCallbackConfig = Async.concurrently saveHash' executeCallbackConfig'
+                if hashesAreDifferent then CallbackCalled response' <$ saveHashAndExecuteCallbackConfig 
+                                      else pure $ CallbackNotCalled response'
 
 repeatScrape :: Hashable t => CronSchedule -> ScrapeConfig t -> Scraper t -> Either [ValidationError] (IO ())
 repeatScrape cs sc s = 
@@ -55,7 +56,7 @@ repeatScrape cs sc s =
   where repeatScrape' :: IO () -> IO ()
         repeatScrape' scrapeAction = () <$ CronSchedule.execSchedule (CronSchedule.addJob scrapeAction cs)
 
-scrapeAll :: Hashable t => [(ScrapeConfig t, Scraper t)] -> [(Url, Either [ValidationError] (IO t))]
+scrapeAll :: Hashable t => [(ScrapeConfig t, Scraper t)] -> [(Url, Either [ValidationError] (IO (ScrapeResult t)))]
 scrapeAll infos = let responses = TU.uncurry scrape <$> infos 
                       urls = (^. scrapeInfoUrl) <$> (fst <$> infos)
                   in urls `zip` responses
