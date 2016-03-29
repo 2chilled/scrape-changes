a97dfad6cdf246f338765457736579fabefa5503
Matthias Herrmann <matthias.mh.herrmann@googlemail.com>
2016-03-29 20:11:01 +0200

    Finished examples (so far)

--- scrape-changes-examples/examples.hs
+++ scrape-changes-examples/examples.hs
@@ -7,18 +7,21 @@ import Data.Foldable (find)
 import Data.Maybe (fromMaybe)
 import Text.HTML.TagSoup (Tag(..), (~==), (~/=), parseTags, fromAttrib) 
 import Data.List.NonEmpty (NonEmpty ((:|)))
-import Network.ScrapeChanges
 import qualified System.Log.Logger as Logger
 import qualified System.Log.Handler.Syslog as Syslog
 import Data.Monoid ((<>))
 import Control.Monad (forever)
+import Network.ScrapeChanges
 
 main :: IO ()
 main = do
   _ <- configureLogging
-  _ <- print `either` id $ scrapeChangesJobs 
+  _ <- (Logger.errorM thisLogger . show) `either` id $ scrapeChangesJobs 
   putStrLn "scrape-changes examples executable. Just look at the example source code."
-  forever getLine
+  -- |Simplest way to block the main thread forever. Good enough for the use cases of 'scrape-changes'
+  _ <- forever getLine
+  -- |Will never be executed in this case
+  clearAllScrapeConfigs
 
 -- |Google logo scrape function using the tagsoup library
 scrapeGoogleLogo :: ByteString -> Text
@@ -39,10 +42,10 @@ scrapeChangesJobs = repeatScrapeAll [
     -- |Checks each minute for changes and sends a mail if there are any
     ScrapeSchedule {
       _scrapeScheduleCron = "* * * * *"
-    , _scrapeScheduleConfig = mailScrapeConfig "http://www.google.co.uk" 
+    , _scrapeScheduleConfig = mailScrapeConfig "http://www.google.co.uk" --to scrape
                                                (MailAddr Nothing "max@mustermann.de") --from
                                                (MailAddr Nothing "receiver@scrape-changes.com" :| []) --to
-    , _scrapeScheduleScraper = scrapeGoogleLogo
+    , _scrapeScheduleScraper = scrapeGoogleLogo --scrape function
     }
     -- |Checks each minute for changes and notifies to syslog if there are any
   , ScrapeSchedule {
